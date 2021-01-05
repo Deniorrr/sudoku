298250d0960e78bfb0b69afa77eb82d2cd162f6d
Deniorrr <49251639+Deniorrr@users.noreply.github.com>
2021-01-05 20:50:24 +0100

    New option "find one"

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-class Squares {
+// class Squares {
+//     constructor(){
+//         this.squares = 
+//         [
+//             [0,1,2,9,10,11,18,19,20],
+//             [3,4,5,12,13,14,21,22,23],
+//             [6,7,8,15,16,17,24,25,26],
+//             [27,28,29,36,37,38,45,46,47],
+//             [30,31,32,39,40,41,48,49,50],
+//             [33,34,35,42,43,44,51,52,53],
+//             [54,55,56,63,64,65,72,73,74],
+//             [57,58,59,66,67,68,75,76,77],
+//             [60,61,62,69,70,71,78,79,80]
+//         ];
+//     }
+// }
+
+class Sudoku{
     constructor(){
+        this.inner = "";
+        this.table= [];
+        this.tilemap = [[],[],[],[],[],[],[],[],[],[]];
+        this.filltilemap();
         this.squares = 
         [
             [0,1,2,9,10,11,18,19,20],
@@ -12,77 +33,71 @@ class Squares {
             [57,58,59,66,67,68,75,76,77],
             [60,61,62,69,70,71,78,79,80]
         ];
-    }
-}
-
-class Sudoku{
-    constructor(){
-        this.inner = "";
-        this.table= [];
-        this.tilemap = [[],[],[],[],[],[],[],[],[],[]];
-        this.filltilemap();
-        this.squares = new Squares;
+        this.onetile = false
     }
 
     draw_table(){
         let element = document.getElementById("container");
         this.inner = "<table>";
-        for(let i = 0; i<9; i++)this.draw_row(i);
+        for(let row = 0; row<9; row++)this.draw_row(row);
         this.inner += "</table>";
         element.innerHTML = this.inner;
     }
-    draw_row(i){
+    draw_row(row){
         this.inner += "<tr>"
-        for(let j = 0; j<9; j++)this.draw_cell(i, j);
+        for(let col = 0; col<9; col++)this.draw_cell(row, col);
         this.inner += "</tr>";
     }
-    draw_cell(i, j){
+    draw_cell(row, col){
         //this.inner += "<td>"+ ((i*9)+j) +"</td>"
-        this.inner += "<td><input type='number' min='1' max='9' id='t" + ((i*9)+j) + "'></td>"
+        this.inner += "<td><input type='number' min='1' max='9' id='t" + this.return_tile(row,col) + "'></td>"
     }
     filltilemap(){
-        for(let i = 0; i<10; i++)
+        for(let digit = 0; digit<10; digit++)
         {
-            for(let j = 0; j<81; j++)
+            for(let tile = 0; tile<81; tile++)
             {
-                this.tilemap[i][j] = false
+                this.tilemap[digit][tile] = false
             }
         }  
     }
-    set_column(j){
-        let column = j%9;
+    return_tile(row, col){
+        return ((row*9)+col)
+    }
+    set_column(tile){
+        let column = tile%9;
         for(let i=column; i<81; i+=9)
         {
-                this.tilemap[this.table[j]][i] = true;
+                this.tilemap[this.table[tile]][i] = true;
         }
     }
-    set_row(j){
-        let row = Math.floor(j/9);
+    set_row(tile){
+        let row = Math.floor(tile/9);
         let endofrow = ((9*row)+9);
         for(let i=row*9; i<endofrow; i++)
         {
-                this.tilemap[this.table[j]][i] = true;
+                this.tilemap[this.table[tile]][i] = true;
         }
     }
     set_square(j){
         for(let i = 0; i<9; i++)
         {
-            if(this.squares.squares[i].includes(j))
+            if(this.squares[i].includes(j))
             {
                 for(let k = 0; k<9; k++)
                 {
-                    this.tilemap [this.table[j]][this.squares.squares[i][k]] = true;
+                    this.tilemap [this.table[j]][this.squares[i][k]] = true;
                 }
             }
         }
     }
     assign_tilemap(){
-        for(let j = 0; j<81; j++)
-        if(this.table[j]!=0)
+        for(let tile = 0; tile<81; tile++)
+        if(this.table[tile]!=0)
         {
-            this.set_column(j);
-            this.set_row(j);
-            this.set_square(j);
+            this.set_column(tile);
+            this.set_row(tile);
+            this.set_square(tile);
         } 
     }
     set_table(){
@@ -116,16 +131,41 @@ class Sudoku{
         document.getElementById("wynik").innerHTML = this.inner;
     }
     //solver
-    find_sth(){
-        for(let i=1; i<=9; i++)
+    find_one_tile(k){
+        this.onetile = true
+        for(let k=1; k<=9; k++)
         {
-            this.find_one_tile(i)
+            if(this.onetile)
+            {
+                this.find_inrow(k);
+            }else{
+                break;
+            }
+            if(this.onetile)
+            {
+                this.find_incolumn(k);
+            }else{
+                break;
+            }
+            if(this.onetile)
+            {
+                this.find_insquare(k);
+            }else{
+                break;
+            }
+        }
+        if(this.onetile)
+        {
+            document.getElementById("prompt").innerHTML = "I can't solve this case";
         }
     }
-    find_one_tile(k){
-        this.find_inrow(k);
-        this.find_incolumn(k);
-        //this.find_insquare(k);
+    find_many_tiles(k){
+        for(let k=1; k<=9; k++)
+        {
+            this.find_inrow(k);
+            this.find_incolumn(k);
+            this.find_insquare(k);
+        }
     }
     find_inrow(k){
         for(let j = 0; j<9; j++)
@@ -145,9 +185,8 @@ class Sudoku{
             {
                 document.getElementById("t" + possible_tile).value = k;
                 sudoku.set_table();
-                //return true;
-            }else{
-               // return false;
+                this.onetile = false
+                break;
             }
         }
     }
@@ -169,9 +208,8 @@ class Sudoku{
             {
                 document.getElementById("t" + possible_tile).value = k;
                 sudoku.set_table();
-                //return true;
-            }else{
-                //return false;
+                this.onetile = false
+                break;
             }
         }
     }
@@ -183,20 +221,22 @@ class Sudoku{
             let i = 0;
             for(i = 0; i<9; i++)
             {
-                if(!this.tilemap[k][this.squares.squares[j][i]])
+                console.log(this.squares[j][i])
+                if(!this.tilemap[k][this.squares[j][i]])
                 {
                     possibilities+=1;
-                    possible_tile = ((i*9)+j)
+                    possible_tile = this.squares[j][i]
                 }
             }
             if(possibilities==1)
             {
                 document.getElementById("t" + possible_tile).value = k;
                 sudoku.set_table();
-                return true;
+                this.onetile = false
+                break;
             }
+        console.log("next")
         }
-        return false;
     }
 }
 
@@ -208,8 +248,12 @@ document.getElementById("zapisz").addEventListener('click', ()=>{
 })  
 
 document.getElementById("znajdz").addEventListener('click', ()=>{
-    sudoku.find_sth();
+    sudoku.find_many_tiles();
 })  
+
+document.getElementById("znajdz_jeden").addEventListener('click', ()=>{
+    sudoku.find_one_tile();
+}) 
 
 function b(k){
     sudoku.draw_help(k);
