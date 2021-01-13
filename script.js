d8c30d4baae682e7b81b083a7a93b22e5db605d4
Deniorrr <49251639+Deniorrr@users.noreply.github.com>
2021-01-13 19:46:54 +0100

    Added favicon, changed names to english

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// class Squares {
-//     constructor(){
-//         this.squares = 
-//         [
-//             [0,1,2,9,10,11,18,19,20],
-//             [3,4,5,12,13,14,21,22,23],
-//             [6,7,8,15,16,17,24,25,26],
-//             [27,28,29,36,37,38,45,46,47],
-//             [30,31,32,39,40,41,48,49,50],
-//             [33,34,35,42,43,44,51,52,53],
-//             [54,55,56,63,64,65,72,73,74],
-//             [57,58,59,66,67,68,75,76,77],
-//             [60,61,62,69,70,71,78,79,80]
-//         ];
-//     }
-// }
-
 class Sudoku {
   constructor() {
     this.inner = "";
@@ -127,7 +110,7 @@ class Sudoku {
       this.inner += "</tr>";
     }
     this.inner += "</table>";
-    document.getElementById("wynik").innerHTML = this.inner;
+    document.getElementById("devtool").innerHTML = this.inner;
   }
   //solver
   find_one_tile(k) {
@@ -153,7 +136,7 @@ class Sudoku {
       this.find_intile();
     }
     if (this.onetile) {
-      document.getElementById("prompt").innerHTML = "I can't find anything";
+      document.getElementById("prompt").innerHTML = "I can't find nothing more";
     }
   }
   find_many_tiles(k) {
@@ -252,15 +235,15 @@ class Sudoku {
 const sudoku = new Sudoku;
 sudoku.draw_table();
 
-document.getElementById("zapisz").addEventListener('click', () => {
+document.getElementById("save").addEventListener('click', () => {
   sudoku.set_table();
 })
 
-document.getElementById("znajdz").addEventListener('click', () => {
+document.getElementById("find").addEventListener('click', () => {
   sudoku.find_many_tiles();
 })
 
-document.getElementById("znajdz_jeden").addEventListener('click', () => {
+document.getElementById("find_one").addEventListener('click', () => {
   sudoku.find_one_tile();
 })
 
